075333a0c03f1b1da3ae05c03ec47cd213b3e836
Adam DePaolo <amdepaolo@gmail.com>
2022-05-04 13:16:20 -0400

    commit tree dae5e4fcf3bd162bcb12a01d1dcb41647fd5e5b4

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    for (let j = i+1; j < array.length; j++){
+      if (array[i] + array[j] === target){
+        return true;
+      }
+    }
+  };
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  for each number of array
+    go through all following numbers in array
+    if sum is target
+      return true
+
 */
 
 /*
   Add written explanation of your solution here
+  take the first number of the array and check it against every other number of the array, if the sum is the target have it return true.
+  Move onto comparing the second number with everything then on. Making sure to start ignore the first number
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +44,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([8, 6, 5], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 5, 3, 5, 6, 7, 50, 50], 100));
 }
 
 module.exports = hasTargetSum;
